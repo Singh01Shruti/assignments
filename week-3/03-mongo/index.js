904b089816fe44e1553d0f7392e23aebc9d49a53
Shruti Singh <singh001shruti@gmail.com>
2023-12-21 21:17:25 +0530

    week-3 3rd portion done

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes/admin")
 const userRouter = require("./routes/user");
+const mongoose = require("mongoose");
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-app.listen(3000, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(3000);
+
+mongoose.connect('mongodb+srv://singh001shruti:eoYn0wHIr4puEx0z@cluster0.k9nfaff.mongodb.net/');
