93e161cb0cccbe17da213eada26b3e6ca6f0e8a0
Shruti Singh <singh001shruti@gmail.com>
2023-12-22 11:59:17 +0530

    week-3 done

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
-});
-
-const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username : String,
+    password : String,
+    token : String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title : String,
+    description : String,
+    price : Number,
+    imageLink : String,
+    published : Boolean
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username : String,
+    password : String,
+    token : String,
+    purchasedCourses : [CourseSchema]
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
